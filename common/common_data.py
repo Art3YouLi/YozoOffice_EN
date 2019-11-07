26aa7aff7e9020f1b1c78216700f9c78e67eaf60
Art3YouLi <excel1entxx@126.com>
2019-11-07 15:05:07 +0800

    修改pg切换模式为英文版

--- common/common_data.py
+++ common/common_data.py
@@ -9,17 +9,26 @@ WP = ['wp', 'pg']
 WS = ['wp', 'ss']
 TYPE_LIST = ['all', 'wp', 'ss', 'pg', 'pdf']
 SEARCH_DICT = {'wp': 'docx', 'ss': 'xlsx', 'pg': 'pptx'}
-SWITCH_LIST = ['无切换', '平滑淡出', '从全黑淡出', '切出', '从全黑切出', '溶解', '向下擦除', '向左擦除', '向右擦除',
-               '向上擦除', '扇形展开', '从下抽出', '从左抽出', '从右抽出', '从上抽出', '从左下抽出', '从左上抽出',
-               '从右下抽出', '从右上抽出', '盒状收缩', '盒状展开', '1根轮辐', '2根轮辐', '3根轮辐', '4根轮辐', '8根轮辐',
-               '上下收缩', '上下展开', '左右收缩', '左右展开', '左下展开', '左上展开', '右下展开', '右上展开', '圆形',
-               '菱形', '加号', '新闻快报', '向下推出', '向左推出', '向右推出', '向上推出', '向下插入', '向左插入',
-               '向右插入', '向上插入', '向左下插入', '向左上插入', '向右下插入', '向右上插入', '水平百叶窗',
-               '垂直百叶窗', '横向棋盘式', '纵向棋盘式', '水平梳理', '垂直梳理', '水平线条', '垂直线条', '随机']
+SWITCH_LIST = [' None ', ' Fade ', ' Fade Through Black ', ' Cut ', ' Cut Through Black ', ' Dissolve ',
+               ' Wipe from Bottom ', ' Wipe from Left ', ' Wipe from Right ', ' Wipe from Top ',
+               ' Wheel ', ' Pull from Bottom ', ' Pull from Left ', ' Pull from Right ', ' Pull from Top ',
+               ' Pull from Bottom-Left ', ' Pull from Top-Left ', ' Pull from Buttom-Right ', ' Pull from Top-Right ',
+               ' Box Shrink ', ' Box Expand ', ' 1 Spoke ', ' 2 Spokes ', ' 3 Spokes ', ' 4 Spokes ', ' 8 Spokes ',
+               ' Vertical In ', ' Vertical Out ', ' Horizontal In ', ' Horizontal Out ', ' Expand from Bottom-Left ',
+               ' Expand from Top-Left ', ' Expand from Bottom-Right ', ' Expand from Top-Right ', ' Circle ',
+               ' Diamond ', ' Plus ', ' Flash ', ' Push from Bottom ', ' Push from Left ', ' Push from Right ',
+               ' Push from Top ', ' Insert From Bottom ', ' Insert From Left ', ' Insert From Right ',
+               ' Insert From Top ', ' Insert From Bottom-Left ', ' Insert From Top-Left ', ' Insert From Bottom-Right ',
+               ' Insert From Top-Right ', ' Horizontal Blinds ', ' Vertical Blinds ', ' Horizontal Checkerboard ',
+               ' Vertical Checkerboard ', ' Horizontal Comb ', ' Vertical Comb ', ' Horizontal Bars ',
+               ' Vertical Bars ', ' Random ']
 FOLDER_LIST = ['Moblie Phones', 'My Documents', 'Download', 'QQ', 'Wechat']
 INDEX_SHARE_LIST = ['qq', 'wechat', 'email', 'more']
 AUTO_SUM = ['求和', '平均值', '计数', '最大值', '最小值']
-CASE_FILES = ['test_homepage.py', 'test_common.py', 'test_wp.py', 'test_ss.py', 'test_pg.py']
+FORMAT = [' Title and Subtitle ', ' Title ', ' Title and Text ', ' Title and Two Column Text ',
+          ' Title and Vertical Text ', ' Vertical Title and Text ', ' Blank ', ' Title and Picture ',
+          ' Title, Text and Picture ', ' Title, Picture and Text ', ' Title, Picture and Vertical Text ',
+          ' Title, Vertical Text and Picture ']
 
 # File path
 CSV_FILE = '../data/account.csv'
@@ -27,4 +36,8 @@ CASE_DIR = '../test_case'
 REPORT_DIR = '../reports'
 CON_LOG = '../config/logs.conf'
 CAPS_YAML = '../config/yozo_office_caps.yaml'
+SEREEN_SHOTS = '../screenshots/'
+
+# Test case
+CASE_FILES = ['test_homepage.py', 'test_common.py', 'test_wp.py', 'test_ss.py', 'test_pg.py']
 
