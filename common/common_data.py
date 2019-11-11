e28248492537760a169a668b175fbad374031a1b
Art3YouLi <excel1entxx@126.com>
2019-11-11 18:17:51 +0800

    增加共用数据

--- common/common_data.py
+++ common/common_data.py
@@ -9,6 +9,21 @@ WP = ['wp', 'pg']
 WS = ['wp', 'ss']
 TYPE_LIST = ['all', 'wp', 'ss', 'pg', 'pdf']
 SEARCH_DICT = {'wp': 'docx', 'ss': 'xlsx', 'pg': 'pptx'}
+FOLDER_LIST = ['Moblie Phones', 'My Documents', 'Download', 'QQ', 'Wechat']
+INDEX_SHARE_LIST = ['qq', 'wechat', 'email', 'more']
+
+# SS
+AUTO_SUM = [' AutoSum ', ' Average ', ' Count ', ' Max ', ' Min ']
+DATE_FILTER = ['等于', '不等于', '在以下日期之后', '在以下日期之后或与之相同', '在以下日期之前',
+               '在以下日期之钱或与之相同', '开始于', '非开始于', '结束于', '非结束于', '包含', '不包含']
+NUM_FILTER = ['等于','不等于','大于','大于等于','小于','小于等于','开始于','非开始于','结束于','非结束于','包含','不包含']
+TEXT_FILTER = ['等于','不等于','大于','大于等于','小于','小于等于','开始于','非开始于','结束于','非结束于','包含','不包含']
+
+# PG
+FORMAT = [' Title and Subtitle ', ' Title ', ' Title and Text ', ' Title and Two Column Text ',
+          ' Title and Vertical Text ', ' Vertical Title and Text ', ' Blank ', ' Title and Picture ',
+          ' Title, Text and Picture ', ' Title, Picture and Text ', ' Title, Picture and Vertical Text ',
+          ' Title, Vertical Text and Picture ']
 SWITCH_LIST = [' None ', ' Fade ', ' Fade Through Black ', ' Cut ', ' Cut Through Black ', ' Dissolve ',
                ' Wipe from Bottom ', ' Wipe from Left ', ' Wipe from Right ', ' Wipe from Top ',
                ' Wheel ', ' Pull from Bottom ', ' Pull from Left ', ' Pull from Right ', ' Pull from Top ',
@@ -22,13 +37,6 @@ SWITCH_LIST = [' None ', ' Fade ', ' Fade Through Black ', ' Cut ', ' Cut Throug
                ' Insert From Top-Right ', ' Horizontal Blinds ', ' Vertical Blinds ', ' Horizontal Checkerboard ',
                ' Vertical Checkerboard ', ' Horizontal Comb ', ' Vertical Comb ', ' Horizontal Bars ',
                ' Vertical Bars ', ' Random ']
-FOLDER_LIST = ['Moblie Phones', 'My Documents', 'Download', 'QQ', 'Wechat']
-INDEX_SHARE_LIST = ['qq', 'wechat', 'email', 'more']
-AUTO_SUM = ['求和', '平均值', '计数', '最大值', '最小值']
-FORMAT = [' Title and Subtitle ', ' Title ', ' Title and Text ', ' Title and Two Column Text ',
-          ' Title and Vertical Text ', ' Vertical Title and Text ', ' Blank ', ' Title and Picture ',
-          ' Title, Text and Picture ', ' Title, Picture and Text ', ' Title, Picture and Vertical Text ',
-          ' Title, Vertical Text and Picture ']
 
 # File path
 CSV_FILE = '../data/account.csv'
