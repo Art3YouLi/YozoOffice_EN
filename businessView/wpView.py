c052b4dc9bded59ef5f7dea0316c7996feb67f52
Art3YouLi <excel1entxx@126.com>
2019-12-05 14:11:21 +0800

    1.更新template_object（还是有问题，后期优化） 2.新增方法

--- businessView/wpView.py
+++ businessView/wpView.py
@@ -107,8 +107,146 @@ class WPView(GeneralView):
         parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_insert_table')
         parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')[0].click()
 
-    def template_object(self, filename):
+    def template_object(self, filename, target_pos=5, record_pos=None):
         pro_path = get_project_path()
         clickpic_path = os.path.join(pro_path, 'clickPicture_EN')
-        t = Template(os.path.join(clickpic_path, filename), resolution=(1080, 1920), rgb=True, threshold=0.5)
+        # 图像识别阈值：threshold
+        # 图像识别新增参数:target_pos 图像点击位置
+        # 是否灰度：rgb
+        # 图片位置：record_pos  优先在录制时的位置附近查找，如果找不到符合条件的图片，再扩大寻找范围到整个画面
+        t = Template(os.path.join(clickpic_path, filename), resolution=(1080, 1920), rgb=True, target_pos=target_pos,
+                     threshold=0.8, record_pos=record_pos)
+        time.sleep(0.5)
         return t
+
+    def table_merge_split(self):
+        print(self.driver.find_element(By.ID,"com.yozo.office.en:id/yozo_ui_wp_option_id_merge_cell_textview").get_attribute('text'))
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_merge_cell_textview').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_split_cell_textview').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/btn_cancle').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_split_cell_textview').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/et_row').set_text('2')
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/et_column').set_text('2')
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/btn_ok').click()
+
+    def table_insert_list(self):
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_insert_table')
+        childs = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        c_1 = childs[-1]
+        c_1.click()
+        parent = self.driver.find_element(By.XPATH,
+                                          '//*[@resource-id="com.yozo.office.en:id/yozo_ui_option_content_container"]')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        count = 0
+        for i in childs0:
+            i.click()
+            count += 1
+            print("正在插入第%s个表格" % count)
+            self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_toolbar_button_undo').click()
+            self.group_button_click(' Insert ')
+            c_1.click()
+        s = self.swipe_option('up')
+        self.swipe(s[0], s[1], s[2], s[3])
+        self.swipe(s[0], s[1], s[2], s[3])
+        childs1 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        for i in childs1[10:]:
+            i.click()
+            count += 1
+            print("正在插入第%s个表格" % count)
+            self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_toolbar_button_undo').click()
+            self.group_button_click(' Insert ')
+            c_1.click()
+
+    def table_type_list(self):
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_table_style')
+        childs = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs))
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_options_table_style_all')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs0))
+        table_list0 = (40 - len(childs0)) / 5
+        s = self.swipe_option('up')
+        if int(table_list0) != 0:
+            self.swipe(s[0], s[1], s[2], s[3])
+            list(map(lambda i: parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')[i].click(),
+                     range(-(int(table_list0) * 5), 0)))
+
+    def table_fill_color(self):  # 填充色
+        # 设置表格填充颜色，并返回自定义色值
+        logging.info('==========table_fill_color==========')
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_cells_fill_color')
+        childs = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs))
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_id_color_all')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs0))
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_group_button_main_text_view').click()
+        self.driver.find_element(By.ID, "com.yozo.office.en:id/color_picker_view").click()
+        free_col = self.driver.find_element(By.ID, "com.yozo.office.en:id/tv_hex").get_attribute('text')
+
+        self.driver.find_element(By.ID, "com.yozo.office.en:id/yozo_ui_full_screen_base_dialog_id_ok").click()
+        return free_col
+
+    def table_border_line(self):  # 边框线
+        logging.info('==========table_border_line==========')
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_table_border')
+        childs = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs))
+
+        # # 边框线颜色
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_ss_option_id_cell_border_color').click()
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_id_color_all')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs0))
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_back_button').click()
+        # 边框线线条样式
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_ss_option_id_cell_border_style').click()
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_content_container')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        print(len(childs0))
+        list(map(lambda i: i.click(), childs0))
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_back_button').click()
+        # # 边框线样式
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_ss_option_id_cell_border_group_all')
+        childs0 = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        list(map(lambda i: i.click(), childs0))
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_back_button').click()
+
+    def table_insert_row_col(self, direction=''):
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_table_option_id_insert_table').click()
+        row_col = ['up', 'down', 'left', 'light']
+        self.driver.find_elements(By.ID,'com.yozo.office.en:id/label_text_view')[row_col.index(direction)].click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_back_button').click()
+
+    def table_delete_row_col_all(self, del0=''):
+        ele_a = '//*[@resource-id="com.yozo.office.en:id/yozo_ui_wp_table_option_id_insert_table"]'
+        ele_b = '//*[@resource-id="com.yozo.office.en:id/yozo_ui_wp_option_id_table_border"]'
+        self.swipe_ele(ele_a, ele_b)
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_table_option_id_delete_table').click()
+        del_ = ['row', 'col', 'all']
+        self.driver.find_elements(By.ID,'com.yozo.office.en:id/label_text_view')[del_.index(del0)].click()
+        if del0 != 'all':
+            self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_back_button').click()
+
+    def chart_insert_list(self, chart_type):
+        parent = self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_option_content_container')
+        childs = parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')
+        count = 0
+        for i in range(len(childs)):
+            parent.find_elements(By.CLASS_NAME, 'android.widget.FrameLayout')[i].click()
+            count += 1
+            logging.info('正在插入第%s个%s' % (count, chart_type))
+            self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_toolbar_button_undo').click()
+            self.group_button_click(' Insert ')
+            s = self.swipe_option('up')
+            while not self.exist('//*[@text=" Chart "]'):
+                self.swipe(s[0], s[1], s[2], s[3])
+            self.driver.find_element(By.XPATH, '//*[@text=" Chart "]').click()
+            while not self.exist('//*[@text="%s"]' % chart_type):
+                self.swipe(s[0], s[1], s[2], s[3])
+            self.driver.find_element(By.XPATH, '//*[@text="%s"]' % chart_type).click()
+
+    def print_long_pic(self):
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/yozo_ui_wp_option_id_export_image').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/rll_export_long_picture_save_layout').click()
+        self.driver.find_element(By.ID, 'com.yozo.office.en:id/rll_export_long_picture_share_layout').click()
